a57619a05f9169ae6cc62c0a04ae46318f5df1fd
Freakkoe <mads.g.j@live.dk>
2024-03-18 22:53:25 +0100

    Added Properties to Model Class Name

--- IMDB_DataInserter&Website/Models/Name.cs
+++ IMDB_DataInserter&Website/Models/Name.cs
@@ -8,5 +8,16 @@ namespace IMDB_DataInserter_Website.Models
 {
     public class Name
     {
+        public string nconst { get; set; }
+        public string primaryName { get; set; }
+        public int? birthYear { get; set; }
+        public int? deathYear { get; set; }
+        public string[]? primaryProfessions { get; set; }
+        public string[]? knownForTitles { get; set; }
+
+        public override string ToString()
+        {
+            return $"nconst: {nconst}, Name: {primaryName}, Birth: {birthYear}, Death {deathYear}, prof: {primaryProfessions?.Length}, titles: {knownForTitles?.Length}";
+        }
     }
 }
