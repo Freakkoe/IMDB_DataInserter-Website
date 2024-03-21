c01e7015f6a42f38327cc35aad7c9d0038670458
Freakkoe <mads.g.j@live.dk>
2024-03-21 22:20:44 +0100

    Created Class Interface and Class Service

---
+++ IMDB_Website/Services/Interface.cs
@@ -0,0 +1,16 @@
+﻿using IMDB_Website.Models;
+
+namespace IMDB_Website.Services
+{
+    public interface Interface
+    {
+        List<Title> GetTopTitles(int amount);
+
+        List<Title> SearchTitles(string criteria);
+
+        Title FindTitleByTconst(string tconst);
+        List<Name> GetTopNames(int amount);
+
+        List<Name> SearchNames(string criteria);
+    }
+}
---
+++ IMDB_Website/Services/Service.cs
@@ -0,0 +1,32 @@
+﻿using IMDB_Website.Models;
+
+namespace IMDB_Website.Services
+{
+    public class Service : Interface
+    {
+        public Title FindTitleByTconst(string tconst)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Name> GetTopNames(int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Title> GetTopTitles(int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Name> SearchNames(string criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Title> SearchTitles(string criteria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
