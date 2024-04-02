df7397c89940766bd88d2bd3367ec3091a27fb11
Freakkoe <mads.g.j@live.dk>
2024-04-02 20:27:44 +0200

    Code Comments

--- IMDB_Website/Services/Interface.cs
+++ IMDB_Website/Services/Interface.cs
@@ -4,22 +4,17 @@ namespace IMDB_Website.Services
 {
     public interface Interface
     {
+        // Title services
         List<Title> GetTopTitles(int amount);
-
         List<Title> SearchTitles(string criteria);
-
         Title FindTitleByTconst(string tconst);
-
         void AddTitle(Title title, string genres);
-
         void UpdateTitle(Title title, string genres);
-
         void DeleteTitle(string tconst);
 
+        // Name services
         List<Name> GetTopNames(int amount);
-
         List<Name> SearchNames(string criteria);
-
         void AddName(Name name, string professions);
     }
 }
--- IMDB_Website/Services/Service.cs
+++ IMDB_Website/Services/Service.cs
@@ -7,27 +7,33 @@ namespace IMDB_Website.Services
 {
     public class Service : Interface
     {
-
+        // Context for interacting with the database
         private readonly imdbContext context;
 
+        // Constructor to initialize the Service with the provided imdbContext
         public Service(imdbContext service)
         {
             context = service;
         }
 
-        #region Title Services
+        // Method to retrieve top titles from the database
         public List<Title> GetTopTitles(int amount)
         {
+            // Retrieve top titles including associated genres, ordered by Tconst, and take specified amount
             return context.Titles.Include(t => t.Genres).OrderByDescending(t => t.Tconst).Take(amount).ToList();
         }
 
+        // Method to find a title by its Tconst
         public Title FindTitleByTconst(string tconst)
         {
+            // Find a title by Tconst including associated genres
             return context.Titles.Where(t => t.Tconst == tconst).Include(t => t.Genres).FirstOrDefault() ?? throw new Exception();
         }
 
+        // Method to search titles based on criteria
         public List<Title> SearchTitles(string criteria)
         {
+            // Perform a stored procedure to find titles based on criteria and transform the results into Title objects
             Task<List<findTitleResult>> task = context.Procedures.findTitleAsync(criteria);
             task.Wait();
             List<findTitleResult> resultList = task.Result;
@@ -48,8 +54,10 @@ namespace IMDB_Website.Services
             return list;
         }
 
+        // Method to add a new title to the database
         public void AddTitle(Title title, string genres)
         {
+            // Perform a stored procedure to add a new title with associated genres
             context.Procedures.addTitleAsync(
                 title.TitleType,
                 title.PrimaryTitle,
@@ -61,8 +69,10 @@ namespace IMDB_Website.Services
                 genres).Wait();
         }
 
+        // Method to update an existing title in the database
         public void UpdateTitle(Title title, string genres)
         {
+            // Perform a stored procedure to update an existing title with associated genres
             context.Procedures.updateTitleAsync(
                 title.Tconst,
                 title.TitleType,
@@ -76,15 +86,17 @@ namespace IMDB_Website.Services
                 ).Wait();
         }
 
+        // Method to delete a title from the database
         public void DeleteTitle(string tconst)
         {
+            // Perform a stored procedure to delete a title
             context.Procedures.deleteTitleAsync(tconst).Wait();
         }
-        #endregion
 
-        #region Name Services
+        // Method to add a new name to the database
         public void AddName(Name name, string professions)
         {
+            // Perform a stored procedure to add a new name with associated professions
             context.Procedures.addNameAsync(
                 name.PrimaryName,
                 name.BirthYear,
@@ -92,13 +104,17 @@ namespace IMDB_Website.Services
                 professions).Wait();
         }
 
+        // Method to retrieve top names from the database
         public List<Name> GetTopNames(int amount)
         {
+            // Retrieve top names including associated professions, ordered by Nconst, and take specified amount
             return context.Names.Include(n => n.Professions).OrderByDescending(n => n.Nconst).Take(amount).ToList();
         }
 
+        // Method to search names based on criteria
         public List<Name> SearchNames(string criteria)
         {
+            // Perform a stored procedure to find names based on criteria and transform the results into Name objects
             Task<List<findNameResult>> task = context.Procedures.findNameAsync(criteria);
             task.Wait();
             List<findNameResult> resultList = task.Result;
@@ -114,11 +130,11 @@ namespace IMDB_Website.Services
 
             return list;
         }
-        #endregion
 
-        #region Generic
+        // Helper method to transform a list of items using a specified transformer function
         private static List<V> TransformItems<T, V>(List<T> items, Func<T, V> transformer)
         {
+            // Transform each item in the list using the provided transformer function
             List<V> transformedItems = new();
             foreach (T item in items)
             {
@@ -128,6 +144,5 @@ namespace IMDB_Website.Services
 
             return transformedItems;
         }
-        #endregion
     }
 }
